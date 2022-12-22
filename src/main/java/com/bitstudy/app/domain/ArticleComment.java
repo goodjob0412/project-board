cd7f9279b0b01ffde8c5531b2bed7eca27dd6cff
nulldefined <goodjob0412@gmail.com>
2022-12-22 20:59:10 +0900

    #15 - 도메인 코드 엔티티로 변환
    1 도메인 클래스 = 1 테이블로 매핑.
    생성자, 'equals()', 'hashcode()' 등 설정

--- src/main/java/com/bitstudy/app/domain/ArticleComment.java
+++ src/main/java/com/bitstudy/app/domain/ArticleComment.java
@@ -26,13 +26,6 @@ public class ArticleComment {
     @Setter
     @ManyToOne(optional = false) // 필수 값이라는 뜻
     private Article article;
-    /* 연관관계 맵핑
-          연관관계 없이 만들면 private Long articleId; 이런식으로 (관계형 데이터베이스 스타일) 하면 된다.
-          그런데 우리는 Article 과 ArticleComment 가 관계를 맺고 있는걸 객체지향적으로 표현하려고 이렇게 쓸거다.
-          그러기 위해서 필요한 건 단방향 이라는 뜻의 @ManyToOne 어노테이션을 써주고,
-          이건 필수 값이다. 라는 뜻으로 (optional = false) 걸어주기
-          댓글은 여러개 : 게시글 1개  == N : 1 == ManyToOne
-    */
 
 
     @Setter
